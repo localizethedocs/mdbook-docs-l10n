88bd578a40ea7f5bba84f525ff12b53fad356c79
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-04 11:37:52 +0000

    Deploying to pages from @ localizethedocs/mdbook-docs-l10n@8cc6135dbf98a9e67b5ce8d603e1cd192d62733e 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["0.5",     "Release 0.5"],
+    ["0.4",     "Release 0.4"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/mdbook-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/mdbook-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/mdbook-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/mdbook-docs-l10n"],
+  ]
+};
